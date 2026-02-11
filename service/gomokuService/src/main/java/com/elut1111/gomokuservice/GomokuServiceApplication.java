304dfb4142420a922f5bf21b8dd300ff9baa88c6
yszmnl <14936579+z5418142@user.noreply.gitee.com>
2026-02-11 15:57:30 +0800

    01

---
+++ service/gomokuService/src/main/java/com/elut1111/gomokuservice/GomokuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elut1111.gomokuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomokuServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GomokuServiceApplication.class, args);
+    }
+
+}
---
+++ service/gomokuService/src/test/java/com/elut1111/gomokuservice/GomokuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elut1111.gomokuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomokuServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
